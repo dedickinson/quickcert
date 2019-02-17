1e326ab3a2c6d4254a337fd835d9badaf59a5626
Duncan Dickinson <duncan@dickinson.name>
2019-02-17 13:13:12 +1000

    Additional exceptions

--- quickcert/exceptions.py
+++ quickcert/exceptions.py
@@ -1,3 +1,11 @@
 class InvalidCertificateTypeException(Exception):
     def __init__(self, message):
         super().__init__(message)
+
+class CertificateEntryNotFoundException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class CertificateAlreadyExistsException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
