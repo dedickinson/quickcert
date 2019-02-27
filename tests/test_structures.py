9b76850ff13e3a86edd85a8389b6a2d9b10173dc
Duncan Dickinson <duncan@dickinson.name>
2019-02-27 18:32:32 +1000

    Added tests

--- tests/test_structures.py
+++ tests/test_structures.py
@@ -1,7 +1,53 @@
-from quickcert.implementations.structures import CertificateStoreEntryImpl
 import pytest
 
-@pytest.mark.skip(reason="not yet implemented")
-def test_create(self):
-    #entry = CertificateStoreEntryImpl()
-    pass
+from quickcert.implementations import (CONST_DEFAULT_ENCRYPTION_PADDING,
+                                       RsaKeyMinter, x509CertificateMinter,
+                                       x509CertificateNameAttributes,
+                                       x509RootCertificateType)
+from quickcert.implementations.structures import CertificateStoreEntryImpl
+from quickcert.interfaces.key import PrivateKey
+
+
+# @pytest.mark.skip(reason="not yet implemented")
+def test_create_CertificateStoreEntryImpl():
+    minter = x509CertificateMinter()
+
+    key_minter = RsaKeyMinter()
+    key: PrivateKey = key_minter.mint()
+
+    attributes = x509CertificateNameAttributes(
+        country_name='AU',
+        state_name='Queensland',
+        locality_name='Brisbane',
+        organization_name='Test Co',
+        common_name='example.com'
+    )
+
+    certificate_type = x509RootCertificateType()
+
+    minter_args = minter.prepare_mint_args(
+        certificate_type=certificate_type,
+        private_key=key,
+        subject=attributes,
+        issuer=attributes
+    )
+
+    certificate = minter.mint(**minter_args)
+    certificate_name = 'Test Certificate'
+
+    entry = CertificateStoreEntryImpl(
+        name=certificate_name,
+        certificate_type=certificate_type,
+        certificate=certificate,
+        key=key
+    )
+
+    assert(entry.name == certificate_name)
+
+    secret_message = b'hello, world'
+    secret_ciphertext = key.public_key().encrypt(
+        plaintext=secret_message, padding=CONST_DEFAULT_ENCRYPTION_PADDING)
+    assert(entry.private_key.decrypt(ciphertext=secret_ciphertext,
+                                     padding=CONST_DEFAULT_ENCRYPTION_PADDING) == secret_message)
+
+    assert(entry.certificate_type.name == certificate_type.name)
--- tests/test_x509_minter.py
+++ tests/test_x509_minter.py
@@ -1,19 +1,16 @@
-from quickcert.implementations import (x509CertificateMinter,
+from quickcert.implementations import (RsaKeyMinter, x509CertificateMinter,
                                        x509CertificateNameAttributes,
                                        x509ClientCertificateType,
                                        x509IntermediateCertificateType,
                                        x509RootCertificateType,
-                                       x509ServerCertificateType,
-                                       RsaKeyMinter)
+                                       x509ServerCertificateType)
 
 
-def test_cert_types():
-    assert(x509RootCertificateType())
-    assert(x509IntermediateCertificateType())
-    assert(x509ServerCertificateType())
-    assert(x509ServerCertificateType())
-
-    assert(x509CertificateMinter())
+def test_cert_type_names():
+    assert(x509RootCertificateType().name == 'root')
+    assert(x509IntermediateCertificateType().name == 'intermediate')
+    assert(x509ServerCertificateType().name == 'server')
+    assert(x509ClientCertificateType().name == 'client')
 
 
 def test_basic_mint():
@@ -23,12 +20,12 @@ def test_basic_mint():
     key = key_minter.mint()
 
     attributes = x509CertificateNameAttributes(
-            country_name='AU',
-            state_name='Queensland',
-            locality_name='Brisbane',
-            organization_name='Test Co',
-            common_name='example.com'
-        )
+        country_name='AU',
+        state_name='Queensland',
+        locality_name='Brisbane',
+        organization_name='Test Co',
+        common_name='example.com'
+    )
 
     minter_args = minter.prepare_mint_args(
         certificate_type=x509RootCertificateType(),
@@ -40,5 +37,17 @@ def test_basic_mint():
     certificate = minter.mint(**minter_args)
 
     assert(certificate)
-    assert(certificate.public_key.public_numbers() == key.public_key().public_numbers())
+    assert(certificate.public_key.public_numbers()
+           == key.public_key().public_numbers())
+
     assert(certificate.issuer.common_name == attributes.common_name)
+    assert(certificate.issuer.locality == attributes.locality)
+    assert(certificate.issuer.organization == attributes.organization)
+    assert(certificate.issuer.state == attributes.state)
+    assert(certificate.issuer.country == attributes.country)
+
+    assert(certificate.subject.common_name == attributes.common_name)
+    assert(certificate.subject.locality == attributes.locality)
+    assert(certificate.subject.organization == attributes.organization)
+    assert(certificate.subject.state == attributes.state)
+    assert(certificate.subject.country == attributes.country)
